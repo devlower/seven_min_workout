78953d7ba20633cf5a24802f35c628dfc460a176
Tuanne Assenço <tuanne.assenco@gmail.com>
2022-08-25 15:47:02 -0300

    Using ViewBinding and fixing flStart image

--- app/src/main/java/com/devlower/a7min/MainActivity.kt
+++ app/src/main/java/com/devlower/a7min/MainActivity.kt
@@ -1,4 +1,4 @@
-                                                                                                                                                                         package com.devlower.a7min
+package com.devlower.a7min
 
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
@@ -7,18 +7,31 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.devlower.a7min.databinding.ActivityMainBinding
 
-                                                                                                                                                                         class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
         setContentView(R.layout.activity_main)
 
-        val flStartButton: FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener{
-            flStartButton.setBackgroundResource(R.drawable.ic_btn_taped)
+        binding?.flStart?.setOnTouchListener { view, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                view.setBackgroundResource(R.drawable.ic_btn_taped)
+                return@setOnTouchListener true
+            }
+            view.setBackgroundResource(R.drawable.ic_init_btn)
+            false
         }
+    }
 
-        }
+    override fun onDestroy() {
+        super.onDestroy()
 
+        binding = null
+    }
 }
