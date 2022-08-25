43d39cc5a9e59474748338f53cb53b97fc7af5d2
Tuanne Assenço <tuanne.assenco@gmail.com>
2022-08-25 18:26:49 -0300

    Switchin activities

---
+++ app/src/main/java/com/devlower/a7min/ExcerciseActivity.kt
@@ -0,0 +1,11 @@
+package com.devlower.a7min
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExcerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_excercise)
+    }
+}
--- app/src/main/java/com/devlower/a7min/MainActivity.kt
+++ app/src/main/java/com/devlower/a7min/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.devlower.a7min
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
@@ -22,6 +23,9 @@ class MainActivity : AppCompatActivity() {
         binding?.flStart?.setOnTouchListener { view, event ->
             if (event.action == MotionEvent.ACTION_DOWN) {
                 view.setBackgroundResource(R.drawable.ic_btn_taped)
+
+                var intent = Intent(this, ExcerciseActivity::class.java)
+                startActivity(intent)
                 return@setOnTouchListener true
             }
             view.setBackgroundResource(R.drawable.ic_init_btn)
