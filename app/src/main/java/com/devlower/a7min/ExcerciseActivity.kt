e16fdf8ffff34e6fb0e313a2a9f6c58b4b42a967
Tuanne Assenço <tuanne.assenco@gmail.com>
2022-09-05 15:00:56 -0300

    Using CountDownTimers to display the seconds remaining

--- app/src/main/java/com/devlower/a7min/ExcerciseActivity.kt
+++ app/src/main/java/com/devlower/a7min/ExcerciseActivity.kt
@@ -2,11 +2,16 @@ package com.devlower.a7min
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.devlower.a7min.databinding.ActivityExcerciseBinding
 import com.devlower.a7min.databinding.ActivityMainBinding
 
 class ExcerciseActivity : AppCompatActivity() {
     private var binding: ActivityExcerciseBinding? = null
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +27,48 @@ class ExcerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
     }
+
+    private fun setupRestView() {
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExcerciseActivity,
+                    "Here now we will start the exercise.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        binding = null
+    }
+
 }
